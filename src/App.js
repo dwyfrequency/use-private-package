b20a62c1e6019019c1fead3f0919ca2c30d23582
Jack Dwyer <jdwy215@gmail.com>
2019-10-06 18:20:29 -0400

    adding example

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import Foursquare from "foursquare-places";
 
 function App() {
+  const [items, setItems] = useState([]);
+
+  const CLIENT_ID = process.env.REACT_APP_CLIENT_ID;
+  const CLIENT_SECRET = process.env.REACT_APP_CLIENT_SECRET;
+
+  const foursquare = new Foursquare(CLIENT_ID, CLIENT_SECRET);
+
+  const params = {
+    ll: "37.7749,-122.4194",
+    query: "Blue Bottle"
+  };
+
+  useEffect(() => {
+    foursquare.venues.getVenues(params).then(res => {
+      this.setState({ items: res.response.venues });
+    });
+  }, [foursquare, params]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div>Items:</div>
+      {this.state.items.map(item => {
+        return <div key={item.id}>{item.name}</div>;
+      })}
     </div>
   );
 }
