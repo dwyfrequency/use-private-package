cfcc65a53b4915b59c6241bbcf4b53e9d235746c
Jack Dwyer <jdwy215@gmail.com>
2019-10-06 18:39:09 -0400

    simplified example

--- src/index.js
+++ src/index.js
@@ -1,10 +1,38 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import * as serviceWorker from "./serviceWorker";
+import React, { useState, useEffect } from "react";
+import ReactDOM from "react-dom";
+import Foursquare from "foursquare-places";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const App = props => {
+  const [items, setItems] = useState([]);
+
+  const CLIENT_ID = process.env.REACT_APP_CLIENT_ID;
+  const CLIENT_SECRET = process.env.REACT_APP_CLIENT_SECRET;
+
+  const foursquare = new Foursquare(CLIENT_ID, CLIENT_SECRET);
+
+  const params = {
+    ll: "37.7749,-122.4194",
+    query: "Blue Bottle"
+  };
+
+  useEffect(() => {
+    foursquare.venues.getVenues(params).then(res => {
+      setItems(res.response.venues);
+    });
+  }, [foursquare, params]);
+
+  return (
+    <div>
+      <div>Items:</div>
+      {items.map(item => {
+        return <div key={item.id}>{item.name}</div>;
+      })}
+    </div>
+  );
+};
+
+ReactDOM.render(<App />, document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
